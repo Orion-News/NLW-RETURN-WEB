259be662aed196585aac13731e441e8c15a8f554
Orion-News <geeh-eb@Hotmail.com>
2022-05-06 16:12:22 -0300

    second commit: day one in nlw-returns thanks branquelo={Diego Fernandes} KKKKKKK

---
+++ src/components/Widget.tsx
@@ -0,0 +1,21 @@
+import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
+
+export function Widget() {
+  return (
+    <Popover className='absolute bottom-4 right-4'>
+      <Popover.Panel>Hello world</Popover.Panel>
+
+      <Popover.Button className='bg-brand-500 rounded-full px-2 h-10 text-white hover:bg-violet-700 transition-colors flex items-center group'>
+        
+        <ChatTeardropDots className='w-6 h-6'/> 
+        
+        <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700 ease-linear'>
+          <span className='pl-2'></span>
+          Feedback 
+        </span>
+      
+      </Popover.Button>
+    </Popover>
+  )
+}
